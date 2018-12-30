23689c0ff4b64e91abdb4bc0bbae380f146a603b
robertdobson <33889822+robertdobson@users.noreply.github.com>
2018-12-30 14:53:08 -0700

    Added src folder containing Program.cs demo code

---
+++ src/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GenericExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new Result<int> { Success = true, Data = 5 };
+            var result2 = new Result<string> { Success = false, Data = "John" };
+
+            var helper = new ResultPrinter();
+
+            helper.Print(result);
+            Console.ReadLine();
+        }
+    }
+
+    public class Result<T>
+    {
+        public bool Success { get; set; }
+        public T Data { get; set; }
+    }
+
+    public class ResultPrinter
+    {
+        public void Print<T>(Result<T> result)
+        {
+            Console.WriteLine(result.Success);
+            Console.WriteLine(result.Data);
+        }
+    }
+}
